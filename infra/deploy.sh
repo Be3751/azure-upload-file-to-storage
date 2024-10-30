30a8bfd776078659ec489f6dd13fdf0353a95885
Masaya Sonobe <masayasonobe@microsoft.com>
2024-10-31 06:10:56 +0900

    fix deploy shell script

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,8 +1,12 @@
+if [ -f api.zip ]; then
+    rm api.zip
+fi
+
 # for frontend
 cd ../app && npm run build
 cp -r dist/* ../api/public
 
 # for backend
-zip -r api.zip ../api/* -x ".env"
+zip -r api.zip ../api/* -x "../api/.env" -x "../api/node_modules/*"
 az webapp deploy --resource-group jatco-upload-file --name jatco-web --type zip --src-path api.zip
 rm api.zip
